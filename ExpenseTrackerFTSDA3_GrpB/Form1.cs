5f826890e6ef73de3f7759ae92ffe58151c78c61
sreng-step <srengty@gmail.com>
2023-02-10 16:07:20 +0700

    Add button hello.

--- ExpenseTrackerFTSDA3_GrpB/Form1.cs
+++ ExpenseTrackerFTSDA3_GrpB/Form1.cs
@@ -6,5 +6,10 @@ namespace ExpenseTrackerFTSDA3_GrpB
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello");
+        }
     }
 }
